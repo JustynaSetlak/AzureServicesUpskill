ae3d56222e06146bd26b902bb17128f8bd74a8db
JustynaSetlak <justa96.s1@gmail.com>
2019-11-21 18:42:41 +0100

    failing tests added

---
+++ Orders.Tests/OrderControllerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class OrderControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
